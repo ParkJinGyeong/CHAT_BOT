f7803b56a9878e7f630d2d80923c5aab0b1e960f
ParkJinGyeong <wlsrud0593@naver.com>
2024-07-23 11:13:05 +0900

    메세지 보내기

---
+++ 00_make_url.py
@@ -0,0 +1,29 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+
+URL = f'https://api.telegram.org/bot{TOKEN}'
+
+method = 'getUpdates'
+
+res = requests.get(f'{URL}/{method}')
+
+res_dict = res.json()
+
+user_input = res_dict['result'][-1]['message']['text']
+user_id = res_dict['result'][-1]['message']['from']['id']
+
+
+print(user_id, user_input)
+
+#웹에서 값을 가지고 온 상태 
+#사용자에게 메세지를 보내는 상태
+SEND_MSG_URL = f'{URL}/sendMessage?chat_id={user_id}&text={user_input}'
+for i in range(5):
+    requests.get(SEND_MSG_URL)
+
+#사용자가 한말을 메아리로 따라함 
